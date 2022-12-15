2990c1259da91e080a4dd7aae52a5d16fb297412
Yogesh Kumar <79986875+yogeshkumar-1234@users.noreply.github.com>
2022-12-15 18:37:51 +0530

    added new backend hosting

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://mediabook-yog.herokuapp.com' });
+const API = axios.create({ baseURL: 'https://mediabook-yog.onrender.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
